33cae5833cdcc4c9815a42d446705755a82996b4
anast <anastasialukyanova27@gmail.com>
2024-07-25 21:53:41 +0300

    WH-5-add-Entity

---
+++ WareHouseAPI/WareHouseDAL/Entities/Waiter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WareHouseDAL.Entities;
+
+namespace WareHouseDAL.Entities
+{
+    public class Waiter
+    {
+        public string Id { get; set; } = null!;
+        public string Name { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public double Salary { get; set; }
+        public IEnumerable<WareHouse> WareHouses { get; set; } = null!;
+    }
+}
---
+++ WareHouseAPI/WareHouseDAL/Entities/WareHouse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WareHouseDAL.Entities
+{
+    public class WareHouse
+    {
+        public string Id { get; set; } = null!;
+        public string Name { get; set; } = null!;
+        public Director? Director { get; set; }
+        public IEnumerable<Waiter>? Waiters { get; set; }
+    }
+}
