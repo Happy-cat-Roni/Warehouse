01fe73aac8ed73c2d11715d2e7dbfa3fe3b78cf3
anast <anastasialukyanova27@gmail.com>
2024-07-30 14:24:06 +0300

    WH-6-connect-database

---
+++ WareHouseAPI/WareHouseAPI/DI/DependencyRegistration.cs
@@ -0,0 +1,12 @@
+﻿using WareHouseBLL.DI;
+
+namespace WareHouseAPI.DI
+{
+    public static class DependencyRegistration
+    {
+        public static void AddApiDependencies(this IServiceCollection services, string? connectionString)
+        {
+            services.AddBLLDependencies(connectionString);
+        }
+    }
+}
