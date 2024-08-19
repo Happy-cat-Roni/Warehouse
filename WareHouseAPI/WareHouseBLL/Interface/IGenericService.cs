574a4e7c1096a38b49a64b6e368f3c6588751bcc
anast <anastasialukyanova27@gmail.com>
2024-08-19 22:20:05 +0300

    WH-9-generic-service

---
+++ WareHouseAPI/WareHouseBLL/Interface/IGenericService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WareHouseBLL.Models;
+using WareHouseDAL.Entities;
+
+namespace WareHouseBLL.Interface
+{
+    public interface IGenericService<TModel> where TModel : BaseModel
+    {
+        Task Create(TModel model, CancellationToken cancellationToken);
+        Task Delete(string id, CancellationToken cancellationToken);
+        Task Update(TModel model, CancellationToken cancellationToken);
+        Task<List<TModel>> GetAll(CancellationToken cancellationToken);
+        Task<TModel?> GetById(string id, CancellationToken cancellationToken);
+    }
+}
